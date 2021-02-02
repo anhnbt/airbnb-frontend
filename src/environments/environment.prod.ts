dcd37e3550d702b9674ddde2261325891c4934cc
anhnbt-it <anhnbt.it@gmail.com>
2021-02-02 16:37:59 +0700

    Add Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8080',
+  firebaseConfig: {
+    apiKey: 'AIzaSyD9hQzPe5z0sMiZwTqDI9Tn_Z7F4lDWElo',
+    authDomain: 'angular-fire-ba00e.firebaseapp.com',
+    projectId: 'angular-fire-ba00e',
+    storageBucket: 'angular-fire-ba00e.appspot.com',
+    messagingSenderId: '850121223897',
+    appId: '1:850121223897:web:975a9750059f579e9e98c8',
+    measurementId: 'G-X2Z2CW6B4G'
+  }
 };
