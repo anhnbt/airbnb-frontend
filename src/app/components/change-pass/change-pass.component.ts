6cb68e18e0326bd6b8c528b8b42a1022bb0073e2
duylue <binhminhbenem2801@gmail.com>
2021-02-19 11:35:29 +0700

    update change password

--- src/app/components/change-pass/change-pass.component.ts
+++ src/app/components/change-pass/change-pass.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../services/user.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 
 @Component({
@@ -13,16 +14,36 @@ login = {
   password: ''
 
 }
-
-  constructor(private userService: UserService) { }
+confirmPw = '';
+check = false;
+checkDisabled = true;
+  message='ĐỔI Mật Khẩu Thành Công';
+  constructor(private userService: UserService,
+              private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
 
+  }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+  checkConfirm():void{
+    if (this.confirmPw !== this.login.password){
+      this.check = true;
+      this.checkDisabled = true
+    }else {
+      if (this.login.password.length<6){
+        this.checkDisabled = true
+      }else {this.checkDisabled = false}
 
+      this.check= false
+    }
   }
 changePw(){
 this.userService.changePassword(this.login).subscribe(res=>{
-  alert('ok')
+
 })
 }
 }
