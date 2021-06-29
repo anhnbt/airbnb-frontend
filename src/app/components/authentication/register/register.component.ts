722a64aa295d57c91740734e5262b026eae5a97a
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-06-29 20:39:16 +0700

    Xóa dấu enter thừa

--- src/app/components/authentication/register/register.component.ts
+++ src/app/components/authentication/register/register.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
